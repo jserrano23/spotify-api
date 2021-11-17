88694e56fc6387bd0b412384fd37475382b91cf7
jserrano5 <54791597+jserrano5@users.noreply.github.com>
2021-11-17 13:20:49 -0800

    modified App.js and Tracks.js to use spotify embeds instead of having to change authorization flows

--- src/App.js
+++ src/App.js
@@ -7,77 +7,38 @@ import Tracks from './pages/Tracks'
 
 const App = () => {
 
-  const getSpotifyAuthResponse = hash => {
-    const response = hash.substring(1);
-    const params = response.split('&');
-    const requestValues = params.reduce((acc, val) => {
-      const [key, value] = val.split('=');
-      acc[key] = value;
-      return acc;
-    }, {});
-
-    return requestValues;
-  }
-
   console.log('Rendering App.js on port 3000');
 
   const tokenUri = 'https://accounts.spotify.com/api/token';
   const baseUri = 'https://api.spotify.com/v1';
 
   const [token, setToken] = useState('')
-  const [tokenType, setTokenType] = useState(false)    // false = client credentials, true = implicit grant
-  const [expiresIn, setExpiresIn] = useState('')
   const [albums, setAlbums] = useState({listOfAlbums: []})
   const [albumInfo, setAlbumInfo] = useState({name: '', imageUrl: '', id: ''})
-  const [tracks, setTracks] = useState({listOfTracks: []})
-  const [currentlyPlaying, setCurrentlyPlaying] = useState()
 
 
   useEffect(() => {
 
-    // if statement to handle the two different authorization methods
-    //   - if hash is found in location -> implicit grant authorization
-    //   - else -> client credentials authorization
-    if(window.location.hash) {
-      console.log('hash found, implementing implicit grant auth')
+    // handle client credentials authorization
+    axios(tokenUri, {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET)
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then (tokenRes => {
+      console.log('token: ' + tokenRes.data.access_token);
+      setToken(tokenRes.data.access_token);
+
+      console.log('client credentials authorization success');
+
+      getNewlyReleased(tokenRes.data.access_token);
       
-      const res = getSpotifyAuthResponse(window.location.hash);
-      console.log('token: ' + res.access_token)
-      
-      setToken(res.access_token);
-      setTokenType(true);
-      setExpiresIn(res.expires_in);
+    })
 
-      console.log('token expires in: ' + expiresIn + ' seconds')
-
-      console.log('implicit grant authorization success');
-
-      getNewlyReleased(res.access_token);
-
-    } else {
-      console.log('no hash found, implementing client credentials auth')
-
-      // handle client credentials authorization
-      axios(tokenUri, {
-        headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded',
-          'Authorization' : 'Basic ' + btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET)
-        },
-        data: 'grant_type=client_credentials',
-        method: 'POST'
-      })
-      .then (tokenRes => {
-        console.log('token: ' + tokenRes.data.access_token);
-        setToken(tokenRes.data.access_token);
-        setTokenType(false);
-
-        console.log('client credentials authorization success');
-
-        getNewlyReleased(tokenRes.data.access_token);
-        
-      })
-    }
-  }, [expiresIn]);
+  }, []);
 
 
   
@@ -101,41 +62,12 @@ const App = () => {
     })
   }
 
-  // getting api data of newly released tracks
-  const getAlbumTracks = val => {
-    axios(baseUri + `/albums/${val}/tracks`, {
-      method: 'GET',
-      headers: {
-        'Authorization' : 'Bearer ' + token
-      }
-    })
-    .then(tracksRes => {
-      setAlbumInfo({
-        name: albumInfo.name,
-        imageUrl: albumInfo.imageUrl,
-        id: val,
-      });
-
-      setTracks({
-        listOfTracks: tracksRes.data.items
-      })
-      console.log('retrieved tracks list')
-      console.log(tracksRes.data.items)
-    })
-  }
-
  
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home albums={albums} setAlbumInfo={setAlbumInfo} tokenType={tokenType} />} />
-
-        {/* need to display specific route based on token type */}
-        {tokenType
-          ? <Route path='/tracksplayer' element={<Tracks albumInfo={albumInfo} tracks={tracks} getTracks={getAlbumTracks} token={token} tokenType={tokenType} setCurrent={setCurrentlyPlaying} current={currentlyPlaying} />} />
-          : <Route path='/tracks' element={<Tracks albumInfo={albumInfo} tracks={tracks} getTracks={getAlbumTracks} token={token} tokenType={tokenType} setCurrent={setCurrentlyPlaying} current={currentlyPlaying} />} />
-        }
-        
+        <Route path='/' element={<Home albums={albums} setAlbumInfo={setAlbumInfo} />} />
+        <Route path='/tracks' element={<Tracks albumInfo={albumInfo} token={token} />} />
       </Routes>
     </div>
   );
--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -1,38 +1,13 @@
 import React from 'react';
 
-import {Typography, Container, Button, Paper, List, ListItem, IconButton} from '@mui/material'
-import {PlayArrow} from '@mui/icons-material'
+import {Container, Button} from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
-import Track from '../components/Track'
 import Navbar from '../components/Navbar'
-import Player from '../components/Player'
 
 const Tracks = props => {
 
-  const scopes = ['streaming', 'user-read-email', 'user-read-private', 'user-read-playback-state', 'user-modify-playback-state']
-  const scopeParams = scopes.join('%20');
-
-  const AUTH_URL = `http://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=token&scope=${scopeParams}&show_dialog=true`;
-
-  const playTrack = track => {
-
-    // false = client credentials, true = implicit grant
-    if (props.tokenType) {
-      console.log('user logged in, able to play track')
-
-      // need to set state of currently playing here
-      props.setCurrent(track)
-
-    } else {
-      const redirectPrompt = window.confirm('To play a track, you must be logged into Spotify. Would you like to be redirected to the Spotify login page?');
-
-      if (redirectPrompt) {
-        window.location = AUTH_URL
-      } 
-    }
-  }
-
+  const embedSrc = `https://open.spotify.com/embed/album/${props.albumInfo.id}`
 
   return (
     <>
@@ -41,34 +16,11 @@ const Tracks = props => {
         <Button my={2} variant='outlined' startIcon={<ArrowBackIosNewIcon />} onClick={() => window.history.back()}>back to albums</Button>
       </Container>
       
-      <Container align='center' onLoad={() => props.getTracks(props.albumInfo.id)}>
+      <Container align='center'>
         {console.log(props.albumInfo.name)}
-        {console.log(props.tracks)}
         
-        <Typography variant='h4'>{props.albumInfo.name}</Typography>
-
-        <Paper align='center'><img src={props.albumInfo.imageUrl} alt={props.albumInfo.name}/></Paper>
-
-        {/* container to display web player */}
-        <Container align='center'>
-          <Player accessToken={props.token} trackUri={props.current?.uri} />
-        </Container>
-
-        <List>
-          {props.tracks.listOfTracks.map((track) => 
-            <ListItem
-              divider
-              key={track.uri}
-              secondaryAction={
-                <IconButton edge='end' onClick={() => playTrack(track)}>
-                  <PlayArrow />
-                </IconButton>
-              }
-            >
-              <Track {...track} />
-            </ListItem>
-          )}
-        </List>
+        {/* generates a spotify embed from the selected album */}
+        <iframe src={embedSrc} title={props.albumInfo.name} width='1200' height='800' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>
         
       </Container>
     </>
